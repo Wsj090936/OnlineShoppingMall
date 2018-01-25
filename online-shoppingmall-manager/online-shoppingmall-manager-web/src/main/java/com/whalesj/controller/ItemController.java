dec8424156685f388d303903d92fc166cfcef912
wushijia <whalewsj@gmail.com>
2018-01-25 19:17:47 +0800

    2018/1/25

--- online-shoppingmall-manager/online-shoppingmall-manager-web/src/main/java/com/whalesj/controller/ItemController.java
+++ online-shoppingmall-manager/online-shoppingmall-manager-web/src/main/java/com/whalesj/controller/ItemController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.whalesj.common.pojo.EasyUiDateGridResult;
+import com.whalesj.common.pojo.TaotaoResult;
 import com.whalesj.pojo.TbItem;
 import com.whalesj.service.ItemService;
 
@@ -26,6 +28,12 @@ public class ItemController {
 	@ResponseBody
 	public EasyUiDateGridResult getItemList(Integer page,Integer rows){
 		EasyUiDateGridResult result = itemService.getItemList(page, rows);
+		return result;
+	}
+	@RequestMapping(value="/item/save",method=RequestMethod.POST)//注意此处是POST请求
+	@ResponseBody
+	public TaotaoResult createItem(TbItem item,String desc){
+		TaotaoResult result = itemService.createItem(item, desc);
 		return result;
 	}
 }
