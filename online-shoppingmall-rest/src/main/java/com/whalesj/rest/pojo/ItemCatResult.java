328c1f52c48e2c3075807e4f149530c43419d517
wushijia <whalewsj@gmail.com>
2018-01-27 18:25:12 +0800

    2018/1/27

---
+++ online-shoppingmall-rest/src/main/java/com/whalesj/rest/pojo/ItemCatResult.java
@@ -0,0 +1,16 @@
+package com.whalesj.rest.pojo;
+
+import java.util.List;
+
+public class ItemCatResult {
+	private List<?> data;
+
+	public List<?> getData() {
+		return data;
+	}
+
+	public void setData(List<?> data) {
+		this.data = data;
+	}
+	
+}
---
+++ online-shoppingmall-rest/src/main/java/com/whalesj/rest/service/ItemCatService.java
@@ -0,0 +1,7 @@
+package com.whalesj.rest.service;
+
+import com.whalesj.rest.pojo.ItemCatResult;
+
+public interface ItemCatService {
+	public ItemCatResult getItemCatList();
+}
