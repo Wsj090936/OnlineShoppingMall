6448a2dae7ffd93819f5593190bc20d7ac840f8b
wushijia <whalewsj@gmail.com>
2018-01-29 16:59:56 +0800

    2018/1/29

---
+++ online-shoppingmall-manager/online-shoppingmall-manager-service/src/main/java/com/whalesj/service/ContentService.java
@@ -0,0 +1,8 @@
+package com.whalesj.service;
+
+import com.whalesj.common.pojo.TaotaoResult;
+import com.whalesj.pojo.TbContent;
+
+public interface ContentService {
+	public TaotaoResult insertContent(TbContent tbContent);
+}
--- online-shoppingmall-portal/src/main/java/com/whalesj/portal/controller/IndexController.java
+++ online-shoppingmall-portal/src/main/java/com/whalesj/portal/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.whalesj.portal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.whalesj.portal.service.ContentService;
 
 /**
  * 首页访问
@@ -10,8 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private ContentService contentService;
+	
 	@RequestMapping("index")
-	public String showIndex(){
+	public String showIndex(Model model){
+		//取轮播图内容
+		String json = contentService.getAdList();
+		//传递给页面
+		model.addAttribute("ad1", json);
 		return "index";
 	}
 }
